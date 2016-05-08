1decd319182759ba55c02f33ac53c7ac142d06aa
Alvarisetty <prabhakar.a25@gmail.com>
2016-05-08 03:07:37 -0700

    Test

--- SeleniumProj/VerifyItems.cs
+++ SeleniumProj/VerifyItems.cs
@@ -32,23 +32,22 @@ namespace SeleniumProj
         [TestMethod]
         public void BankingLogin()
         {
-            //System.Threading.Thread.Sleep(10000);
-            //IWebElement userNameElement = driver.FindElement(By.Id("challengeUsername"));
-            //userNameElement.SendKeys("johnsmith");
-            //IWebElement passwordNameElement = driver.FindElement(By.Id("challengePassword"));
-            //passwordNameElement.SendKeys("Iberia123!");
-            //driver.FindElement(By.ClassName("btn-submit")).Click();
-            //System.Threading.Thread.Sleep(10000);
-            //IWebElement secretQuestionAnswer = driver.FindElement(By.Id("secretQuestionAnswer"));
-            //secretQuestionAnswer.SendKeys("denver");
-            //driver.FindElement(By.ClassName("btn-submit")).Click();
-            //System.Threading.Thread.Sleep(10000);
-            //var description = driver.FindElements(By.CssSelector("#main > div > section > div.row.manage-widgets > div.col-md-5 > div.accounts-view > div > article.internal-accounts > div > ul"));
-            //foreach (IWebElement option in description)
-            //{
-            //    string[] accountDetails = option.Text.Split('\r');
-            //}
-            Assert.IsTrue(true);
+            System.Threading.Thread.Sleep(10000);
+            IWebElement userNameElement = driver.FindElement(By.Id("challengeUsername"));
+            userNameElement.SendKeys("johnsmith");
+            IWebElement passwordNameElement = driver.FindElement(By.Id("challengePassword"));
+            passwordNameElement.SendKeys("Iberia123!");
+            driver.FindElement(By.ClassName("btn-submit")).Click();
+            System.Threading.Thread.Sleep(10000);
+            IWebElement secretQuestionAnswer = driver.FindElement(By.Id("secretQuestionAnswer"));
+            secretQuestionAnswer.SendKeys("denver");
+            driver.FindElement(By.ClassName("btn-submit")).Click();
+            System.Threading.Thread.Sleep(10000);
+            var description = driver.FindElements(By.CssSelector("#main > div > section > div.row.manage-widgets > div.col-md-5 > div.accounts-view > div > article.internal-accounts > div > ul"));
+            foreach (IWebElement option in description)
+            {
+                string[] accountDetails = option.Text.Split('\r');
+            }
         }
     }
 }
